eac003f44e68260d3428f1ce4efaa55f574e853b
egor <eegor650@gmail.com>
2018-03-31 19:20:45 -0700

    dynamodb query subscribers

--- main.go
+++ main.go
@@ -4,50 +4,54 @@ import (
   "fmt"
   "strings"
   "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+  "github.com/aws/aws-sdk-go/service/dynamodb"
   "github.com/aws/aws-sdk-go/aws/session"
   "github.com/aws/aws-sdk-go/service/ses"
-  "github.com/aws/aws-sdk-go/aws/awserr"
   "github.com/aws/aws-lambda-go/lambda"
 )
 
-var recievers = []Reciever{
-	{
-		email: "650egor@gmail.com",
-		subbed: false,
-	},
-	{
-		email: "eegor650@gmail.com",
-		subbed: true,
-	},
-	{
-		email: "broegorov@gmail.com",
-		subbed: false,
-	},
-}
-
-type Reciever struct {
-	email string
-	subbed bool
-}
-
 type Request struct {
 	Commits []struct {
-		Message string `json:"message"`
-    Url string `json:"url"`
-	} `json:"commits"`
+		Message string
+    Url string
+	}
+  Repository struct {
+    Name string
+  }
 }
 
 type Response struct {
-  Url string `json:"url"`
+  Url string
 }
 
-func Email(url string) {
+type Subscribers struct {
+  Email string
+}
+
+
+func GetSubs(repo string) *dynamodb.ScanOutput {
+  svc := dynamodb.New(session.New())
+  input := &dynamodb.ScanInput{
+    ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+      ":r": {
+        S: aws.String(repo),
+      },
+    },
+    FilterExpression:     aws.String("contains (repos, :r)"),
+    ProjectionExpression: aws.String("email"),
+    TableName:            aws.String("webhook-subs"),
+  }
+
+  result, _ := svc.Scan(input)
+  return result
+}
+
+
+func Email(url string, subs []Subscribers) {
   arr := []*string{}
-	for _, r := range(recievers) {
-		if r.subbed {
-      s := aws.String(r.email)
-			arr = append(arr, s)
-		}
+	for _, r := range(subs) {
+		arr = append(arr, aws.String(r.Email))
 	}
   svc := ses.New(session.New())
   input := &ses.SendEmailInput{
@@ -72,40 +76,22 @@ func Email(url string) {
     },
     Source: aws.String("650egor@gmail.com"),
   }
-
-  result, err := svc.SendEmail(input)
-  if err != nil {
-    if aerr, ok := err.(awserr.Error); ok {
-      switch aerr.Code() {
-      case ses.ErrCodeMessageRejected:
-          fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
-      case ses.ErrCodeMailFromDomainNotVerifiedException:
-          fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
-      case ses.ErrCodeConfigurationSetDoesNotExistException:
-          fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
-      case ses.ErrCodeConfigurationSetSendingPausedException:
-          fmt.Println(ses.ErrCodeConfigurationSetSendingPausedException, aerr.Error())
-      case ses.ErrCodeAccountSendingPausedException:
-          fmt.Println(ses.ErrCodeAccountSendingPausedException, aerr.Error())
-      default:
-          fmt.Println(aerr.Error())
-      }
-    } else {
-      fmt.Println(err.Error())
-    }
-    return
-  }
-  fmt.Println(result)
+  svc.SendEmail(input)
 }
+
 
 func Handler(request Request) (Response, error) {
   if strings.Contains(request.Commits[0].Message, "jarvis") {
-    Email(request.Commits[0].Url)
+    subs := GetSubs(request.Repository.Name)
+    items := []Subscribers{}
+    dynamodbattribute.UnmarshalListOfMaps(subs.Items, &items)
+    Email(request.Commits[0].Url, items)
   }
   return Response {
     Url: request.Commits[0].Url,
   }, nil
 }
+
 
 func main() {
   lambda.Start(Handler)
