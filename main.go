d9ff8f62aaeeedfeb9b8c7a8b5fd63b691e46576
egor <eegor650@gmail.com>
2018-04-01 18:19:40 -0700

    get commit diff, add to email body

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  "fmt"
   "strings"
+  "net/http"
+  "io/ioutil"
   "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
   "github.com/aws/aws-sdk-go/service/dynamodb"
@@ -11,6 +12,7 @@ import (
   "github.com/aws/aws-lambda-go/lambda"
 )
 
+// payload from github
 type Request struct {
 	Commits []struct {
 		Message string
@@ -20,18 +22,16 @@ type Request struct {
     Name string
   }
 }
-
-type Response struct {
-  Url string
-}
-
-type Subscribers struct {
+// attributes for each email recipient
+type Subscriber struct {
   Email string
 }
 
-
+// given repo name, return all subscribers
 func GetSubs(repo string) *dynamodb.ScanOutput {
+  // new dynamoDB session
   svc := dynamodb.New(session.New())
+  // scan for all emails who have meet conditions
   input := &dynamodb.ScanInput{
     ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
       ":r": {
@@ -42,57 +42,60 @@ func GetSubs(repo string) *dynamodb.ScanOutput {
     ProjectionExpression: aws.String("email"),
     TableName:            aws.String("webhook-subs"),
   }
-
   result, _ := svc.Scan(input)
   return result
 }
 
-
-func Email(url string, subs []Subscribers) {
+// send email to all subscribers of a repo
+func Email(request Request, subs []Subscriber) {
+  // convert subs to list of aws strings
   arr := []*string{}
 	for _, r := range(subs) {
 		arr = append(arr, aws.String(r.Email))
 	}
+  // get diff
+  res, _ := http.Get(request.Commits[0].Url + ".diff")
+  diff, _ := ioutil.ReadAll(res.Body)
+  // new SES session
   svc := ses.New(session.New())
+  // email body
+  body := "SOMTHING CHANGED: \n" + request.Commits[0].Url + "\n" +
+      "MESSAGE: \n" + request.Commits[0].Message + "\n" +
+      "DIFF: \n" + string(diff) + "\n"
+  // fill out email inputs
   input := &ses.SendEmailInput{
     Destination: &ses.Destination{
       ToAddresses: arr,
     },
     Message: &ses.Message{
       Body: &ses.Body{
-        Html: &ses.Content{
-          Charset: aws.String("UTF-8"),
-          Data:    aws.String(fmt.Sprintf("something changed here: %s", url)),
-        },
         Text: &ses.Content{
           Charset: aws.String("UTF-8"),
-          Data:    aws.String(fmt.Sprintf("something changed here: %s", url)),
+          Data:    aws.String(body),
         },
       },
       Subject: &ses.Content{
         Charset: aws.String("UTF-8"),
-        Data:    aws.String("Test email"),
+        Data:    aws.String(strings.Split(request.Commits[0].Message, "\n")[0]),
       },
     },
     Source: aws.String("650egor@gmail.com"),
   }
+  // send email out
   svc.SendEmail(input)
 }
 
-
-func Handler(request Request) (Response, error) {
+// get subs and email them if condition met
+func Handler(request Request) {
   if strings.Contains(request.Commits[0].Message, "jarvis") {
     subs := GetSubs(request.Repository.Name)
-    items := []Subscribers{}
+    items := []Subscriber{}
     dynamodbattribute.UnmarshalListOfMaps(subs.Items, &items)
-    Email(request.Commits[0].Url, items)
+    Email(request, items)
   }
-  return Response {
-    Url: request.Commits[0].Url,
-  }, nil
 }
 
-
+// call aws lambda function
 func main() {
   lambda.Start(Handler)
 }
