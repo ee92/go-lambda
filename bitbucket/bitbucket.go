aa6242bbc1ca5769b3b46f907e124607940d644f
egor <eegor650@gmail.com>
2018-04-18 19:59:00 -0700

    send email for each commit in each branch

--- bitbucket/bitbucket.go
+++ bitbucket/bitbucket.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "log"
   "time"
   "strings"
   "net/http"
@@ -13,179 +14,183 @@ import (
   "github.com/aws/aws-lambda-go/lambda"
 )
 
+type Commit struct {
+  Hash  string `json:"hash"`
+  Links struct {
+    Self struct {
+      Href string `json:"href"`
+    } `json:"self"`
+    Comments struct {
+      Href string `json:"href"`
+    } `json:"comments"`
+    Patch struct {
+      Href string `json:"href"`
+    } `json:"patch"`
+    HTML struct {
+      Href string `json:"href"`
+    } `json:"html"`
+    Diff struct {
+      Href string `json:"href"`
+    } `json:"diff"`
+    Approve struct {
+      Href string `json:"href"`
+    } `json:"approve"`
+    Statuses struct {
+      Href string `json:"href"`
+    } `json:"statuses"`
+  } `json:"links"`
+  Author struct {
+    Raw  string `json:"raw"`
+    Type string `json:"type"`
+  } `json:"author"`
+  Summary struct {
+    Raw    string `json:"raw"`
+    Markup string `json:"markup"`
+    HTML   string `json:"html"`
+    Type   string `json:"type"`
+  } `json:"summary"`
+  Parents []struct {
+    Type  string `json:"type"`
+    Hash  string `json:"hash"`
+    Links struct {
+      Self struct {
+        Href string `json:"href"`
+      } `json:"self"`
+      HTML struct {
+        Href string `json:"href"`
+      } `json:"html"`
+    } `json:"links"`
+  } `json:"parents"`
+  Date    time.Time `json:"date"`
+  Message string    `json:"message"`
+  Type    string    `json:"type"`
+}
+
+type Change struct {
+  Forced bool `json:"forced"`
+  Old    struct {
+    Type  string `json:"type"`
+    Name  string `json:"name"`
+    Links struct {
+      Commits struct {
+        Href string `json:"href"`
+      } `json:"commits"`
+      Self struct {
+        Href string `json:"href"`
+      } `json:"self"`
+      HTML struct {
+        Href string `json:"href"`
+      } `json:"html"`
+    } `json:"links"`
+    Target struct {
+      Hash  string `json:"hash"`
+      Links struct {
+        Self struct {
+          Href string `json:"href"`
+        } `json:"self"`
+        HTML struct {
+          Href string `json:"href"`
+        } `json:"html"`
+      } `json:"links"`
+      Author struct {
+        Raw  string `json:"raw"`
+        Type string `json:"type"`
+      } `json:"author"`
+      Summary struct {
+        Raw    string `json:"raw"`
+        Markup string `json:"markup"`
+        HTML   string `json:"html"`
+        Type   string `json:"type"`
+      } `json:"summary"`
+      Parents []struct {
+        Type  string `json:"type"`
+        Hash  string `json:"hash"`
+        Links struct {
+          Self struct {
+            Href string `json:"href"`
+          } `json:"self"`
+          HTML struct {
+            Href string `json:"href"`
+          } `json:"html"`
+        } `json:"links"`
+      } `json:"parents"`
+      Date    time.Time `json:"date"`
+      Message string    `json:"message"`
+      Type    string    `json:"type"`
+    } `json:"target"`
+  } `json:"old"`
+  Links struct {
+    Commits struct {
+      Href string `json:"href"`
+    } `json:"commits"`
+    HTML struct {
+      Href string `json:"href"`
+    } `json:"html"`
+    Diff struct {
+      Href string `json:"href"`
+    } `json:"diff"`
+  } `json:"links"`
+  Truncated bool `json:"truncated"`
+  Commits  []Commit  `json:"commits"`
+  Created bool `json:"created"`
+  Closed  bool `json:"closed"`
+  New     struct {
+    Type  string `json:"type"`
+    Name  string `json:"name"`
+    Links struct {
+      Commits struct {
+        Href string `json:"href"`
+      } `json:"commits"`
+      Self struct {
+        Href string `json:"href"`
+      } `json:"self"`
+      HTML struct {
+        Href string `json:"href"`
+      } `json:"html"`
+    } `json:"links"`
+    Target struct {
+      Hash  string `json:"hash"`
+      Links struct {
+        Self struct {
+          Href string `json:"href"`
+        } `json:"self"`
+        HTML struct {
+          Href string `json:"href"`
+        } `json:"html"`
+      } `json:"links"`
+      Author struct {
+        Raw  string `json:"raw"`
+        Type string `json:"type"`
+      } `json:"author"`
+      Summary struct {
+        Raw    string `json:"raw"`
+        Markup string `json:"markup"`
+        HTML   string `json:"html"`
+        Type   string `json:"type"`
+      } `json:"summary"`
+      Parents []struct {
+        Type  string `json:"type"`
+        Hash  string `json:"hash"`
+        Links struct {
+          Self struct {
+            Href string `json:"href"`
+          } `json:"self"`
+          HTML struct {
+            Href string `json:"href"`
+          } `json:"html"`
+        } `json:"links"`
+      } `json:"parents"`
+      Date    time.Time `json:"date"`
+      Message string    `json:"message"`
+      Type    string    `json:"type"`
+    } `json:"target"`
+  } `json:"new"`
+}
+
 // payload from github
 type Request struct {
 	Push struct {
-		Changes []struct {
-			Forced bool `json:"forced"`
-			Old    struct {
-				Type  string `json:"type"`
-				Name  string `json:"name"`
-				Links struct {
-					Commits struct {
-						Href string `json:"href"`
-					} `json:"commits"`
-					Self struct {
-						Href string `json:"href"`
-					} `json:"self"`
-					HTML struct {
-						Href string `json:"href"`
-					} `json:"html"`
-				} `json:"links"`
-				Target struct {
-					Hash  string `json:"hash"`
-					Links struct {
-						Self struct {
-							Href string `json:"href"`
-						} `json:"self"`
-						HTML struct {
-							Href string `json:"href"`
-						} `json:"html"`
-					} `json:"links"`
-					Author struct {
-						Raw  string `json:"raw"`
-						Type string `json:"type"`
-					} `json:"author"`
-					Summary struct {
-						Raw    string `json:"raw"`
-						Markup string `json:"markup"`
-						HTML   string `json:"html"`
-						Type   string `json:"type"`
-					} `json:"summary"`
-					Parents []struct {
-						Type  string `json:"type"`
-						Hash  string `json:"hash"`
-						Links struct {
-							Self struct {
-								Href string `json:"href"`
-							} `json:"self"`
-							HTML struct {
-								Href string `json:"href"`
-							} `json:"html"`
-						} `json:"links"`
-					} `json:"parents"`
-					Date    time.Time `json:"date"`
-					Message string    `json:"message"`
-					Type    string    `json:"type"`
-				} `json:"target"`
-			} `json:"old"`
-			Links struct {
-				Commits struct {
-					Href string `json:"href"`
-				} `json:"commits"`
-				HTML struct {
-					Href string `json:"href"`
-				} `json:"html"`
-				Diff struct {
-					Href string `json:"href"`
-				} `json:"diff"`
-			} `json:"links"`
-			Truncated bool `json:"truncated"`
-			Commits   []struct {
-				Hash  string `json:"hash"`
-				Links struct {
-					Self struct {
-						Href string `json:"href"`
-					} `json:"self"`
-					Comments struct {
-						Href string `json:"href"`
-					} `json:"comments"`
-					Patch struct {
-						Href string `json:"href"`
-					} `json:"patch"`
-					HTML struct {
-						Href string `json:"href"`
-					} `json:"html"`
-					Diff struct {
-						Href string `json:"href"`
-					} `json:"diff"`
-					Approve struct {
-						Href string `json:"href"`
-					} `json:"approve"`
-					Statuses struct {
-						Href string `json:"href"`
-					} `json:"statuses"`
-				} `json:"links"`
-				Author struct {
-					Raw  string `json:"raw"`
-					Type string `json:"type"`
-				} `json:"author"`
-				Summary struct {
-					Raw    string `json:"raw"`
-					Markup string `json:"markup"`
-					HTML   string `json:"html"`
-					Type   string `json:"type"`
-				} `json:"summary"`
-				Parents []struct {
-					Type  string `json:"type"`
-					Hash  string `json:"hash"`
-					Links struct {
-						Self struct {
-							Href string `json:"href"`
-						} `json:"self"`
-						HTML struct {
-							Href string `json:"href"`
-						} `json:"html"`
-					} `json:"links"`
-				} `json:"parents"`
-				Date    time.Time `json:"date"`
-				Message string    `json:"message"`
-				Type    string    `json:"type"`
-			} `json:"commits"`
-			Created bool `json:"created"`
-			Closed  bool `json:"closed"`
-			New     struct {
-				Type  string `json:"type"`
-				Name  string `json:"name"`
-				Links struct {
-					Commits struct {
-						Href string `json:"href"`
-					} `json:"commits"`
-					Self struct {
-						Href string `json:"href"`
-					} `json:"self"`
-					HTML struct {
-						Href string `json:"href"`
-					} `json:"html"`
-				} `json:"links"`
-				Target struct {
-					Hash  string `json:"hash"`
-					Links struct {
-						Self struct {
-							Href string `json:"href"`
-						} `json:"self"`
-						HTML struct {
-							Href string `json:"href"`
-						} `json:"html"`
-					} `json:"links"`
-					Author struct {
-						Raw  string `json:"raw"`
-						Type string `json:"type"`
-					} `json:"author"`
-					Summary struct {
-						Raw    string `json:"raw"`
-						Markup string `json:"markup"`
-						HTML   string `json:"html"`
-						Type   string `json:"type"`
-					} `json:"summary"`
-					Parents []struct {
-						Type  string `json:"type"`
-						Hash  string `json:"hash"`
-						Links struct {
-							Self struct {
-								Href string `json:"href"`
-							} `json:"self"`
-							HTML struct {
-								Href string `json:"href"`
-							} `json:"html"`
-						} `json:"links"`
-					} `json:"parents"`
-					Date    time.Time `json:"date"`
-					Message string    `json:"message"`
-					Type    string    `json:"type"`
-				} `json:"target"`
-			} `json:"new"`
-		} `json:"changes"`
+		Changes []Change `json:"changes"`
 	} `json:"push"`
 	Repository struct {
 		Scm     string `json:"scm"`
@@ -267,20 +272,20 @@ func GetSubs(repo string) *dynamodb.ScanOutput {
 }
 
 // send email to all subscribers of a repo
-func Email(request Request, subs []Subscriber) {
+func Email(c Commit, subs []Subscriber) {
   // convert subs to list of aws strings
   arr := []*string{}
 	for _, r := range(subs) {
 		arr = append(arr, aws.String(r.Email))
 	}
   // get diff
-  res, _ := http.Get(request.Push.Changes[0].New.Target.Links.HTML.Href + "/raw")
+  res, err := http.Get(c.Links.Diff.Href)
   diff, _ := ioutil.ReadAll(res.Body)
   // new SES session
   svc := ses.New(session.New())
   // email body
-  body := "SOMTHING CHANGED: \n" + request.Push.Changes[0].New.Target.Links.HTML.Href + "\n" +
-      "MESSAGE: \n" + request.Push.Changes[0].New.Target.Summary.Raw + "\n" +
+  body := "SOMTHING CHANGED: \n" + c.Links.Self.Href + "\n" +
+      "MESSAGE: \n" + c.Summary.Raw + "\n" +
       "DIFF: \n" + string(diff) + "\n"
   // fill out email inputs
   input := &ses.SendEmailInput{
@@ -296,22 +301,33 @@ func Email(request Request, subs []Subscriber) {
       },
       Subject: &ses.Content{
         Charset: aws.String("UTF-8"),
-        Data:    aws.String(strings.Split(request.Push.Changes[0].New.Target.Summary.Raw, "\n")[0]),
+        Data:    aws.String(strings.Split(c.Summary.Raw, "\n")[0]),
       },
     },
     Source: aws.String("650egor@gmail.com"),
   }
   // send email out
-  svc.SendEmail(input)
+  result, err := svc.SendEmail(input)
+  if err != nil {
+    log.Print(err)
+  } else {
+    log.Print(result)
+  }
 }
 
 // get subs and email them if condition met
 func Handler(request Request) {
-  if strings.Contains(request.Push.Changes[0].New.Target.Summary.Raw, "notify") {
-    subs := GetSubs(request.Repository.Name)
-    items := []Subscriber{}
-    dynamodbattribute.UnmarshalListOfMaps(subs.Items, &items)
-    Email(request, items)
+
+  subs := GetSubs(request.Repository.Name)
+  items := []Subscriber{}
+  dynamodbattribute.UnmarshalListOfMaps(subs.Items, &items)
+
+  for _, change := range(request.Push.Changes) {
+    for _, commit := range(change.Commits) {
+      if strings.Contains(commit.Summary.Raw, "#notify") {
+        Email(commit, items)
+      }
+    }
   }
 }
 
